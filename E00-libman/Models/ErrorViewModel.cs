379fc23439bfcf28e279ce543859ef2b069d4724
Tom Kaeppler <tomkaeppler@web.de>
2022-04-12 16:53:54 +0200

    E00-libman use libman to not push all libs to git

---
+++ E00-libman/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace E00_libman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ E00-libman/README.md
@@ -0,0 +1,7 @@
+# E00-libman
+
+to download all libraries with libman run
+
+```bash
+libman restore
+```
