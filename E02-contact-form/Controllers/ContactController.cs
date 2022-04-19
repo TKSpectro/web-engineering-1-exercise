753aae167aa1dfcc1272c899ee5cfa7b05c0d9ef
Tom Kaeppler <tomkaeppler@web.de>
2022-04-19 18:32:52 +0200

    E02-contact-form add email support

--- E02-contact-form/Controllers/ContactController.cs
+++ E02-contact-form/Controllers/ContactController.cs
@@ -1,10 +1,19 @@
+using System.Net.Mail;
 using E02_contact_form.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E02_contact_form.Controllers
 {
+
     public class ContactController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public ContactController(IConfiguration _configuration)
+        {
+            this.configuration = _configuration;
+        }
+
         public IActionResult Index()
         {
             ViewBag.Title = "Contact";
@@ -16,6 +25,29 @@ namespace E02_contact_form.Controllers
         {
             if (ModelState.IsValid)
             {
+                ViewBag.SentMail = false;
+
+                if (bool.Parse(configuration["smtp:enabled"]))
+                {
+                    SmtpClient smtp = new SmtpClient(configuration["smtp:host"]);
+                    smtp.EnableSsl = bool.Parse(configuration["smtp:enableSsl"]);
+                    smtp.Port = int.Parse(configuration["smtp:port"]);
+                    smtp.Credentials = new System.Net.NetworkCredential(
+                        configuration["smtp:auth:user"], configuration["smtp:auth:pass"]);
+
+                    try
+                    {
+                        smtp.Send(model.Email, "toaddress@somwhere.com",
+                           "Email Subject", $"name:{model.Name}\n email:{model.Email}\n message:{model.Message}");
+
+                        ViewBag.SentMail = true;
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine("Exception caught in CreateMessageWithAttachment()", ex.ToString());
+                    }
+                }
+
                 ViewBag.Title = "Details";
                 return View("Details", model);
             }
