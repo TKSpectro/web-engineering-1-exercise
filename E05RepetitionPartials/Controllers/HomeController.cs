aca2604c6a9ae3893a8baec30990f641280432f3
Tom Kaeppler <tomkaeppler@web.de>
2022-05-17 09:21:40 +0200

    E05RepetitionPartials finished

---
+++ E05RepetitionPartials/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using E05RepetitionPartials.Models;
+
+namespace E05RepetitionPartials.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(Books);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var book = Books.Find((b)=> b.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        
+        return PartialView(book);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    private static List<Book> Books => new List<Book>{
+        new Book{
+            Id = 1,
+            Name = "The Lord of the Rings",
+            Author = "J.R.R. Tolkien",
+            ISBN = "978-0-395-19395-8",
+            Published = new DateOnly(1954, 9, 21),
+            Edition = 1
+        },
+        new Book{
+            Id = 2,
+            Name = "The Hobbit",
+            Author = "J.R.R. Tolkien",
+            ISBN = "978-0-395-19395-8",
+            Published = new DateOnly(1937, 9, 21),
+            Edition = 1
+        },
+        new Book{
+            Id = 3,
+            Name = "Harry Potter and the Philosopher's Stone",
+            Author = "J.K. Rowling",
+            ISBN = "978-0-395-19395-8",
+            Published = new DateOnly(1997, 9, 21),
+            Edition = 1
+        },
+    };
+}
---
+++ E05RepetitionPartials/Models/Book.cs
@@ -0,0 +1,18 @@
+namespace E05RepetitionPartials.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string ISBN { get; set; } = string.Empty;
+
+        public DateOnly Published { get; set; }
+
+        public int Edition { get; set; }
+    }
+}
+
